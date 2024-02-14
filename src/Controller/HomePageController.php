2cfec18e627017c01dabc63b8bf9fd0926f31e76
khoualed2u <rayan.khoualed3@etu.univ-lorraine.fr>
2024-02-14 18:56:16 +0100

    Ajout des annonces dans l'accueil

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -6,14 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\AnnonceService;
+use App\Entity\AnnonceMateriel;
 
 class HomePageController extends AbstractController
 {
     #[Route('', name: 'app_home_page')]
     public function index(EntityManagerInterface $entityManager): Response
     {
+        $annonceService = $entityManager->getRepository(AnnonceService::class)->findAll();
+        $annonceMateriel = $entityManager->getRepository(AnnonceMateriel::class)->findAll();
         return $this->render('home_page/index.html.twig', [
             'controller_name' => 'HomePageController',
+            'annonceService' => $annonceService,
+            'annonceMateriel' => $annonceMateriel,
         ]);
     }
 }
