afc0ddf7d7f722d3ebf30add36c4479e59a136ce
khoualed2u <rayan.khoualed3@etu.univ-lorraine.fr>
2024-02-15 11:57:27 +0100

    rectificatif bug page d'accueil annonces

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -8,14 +8,16 @@ use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\AnnonceService;
 use App\Entity\AnnonceMateriel;
-
+use Doctrine;
 class HomePageController extends AbstractController
 {
     #[Route('', name: 'app_home_page')]
     public function index(EntityManagerInterface $entityManager): Response
     {
         $annonceService = $entityManager->getRepository(AnnonceService::class)->findAll();
+        $entityManager->clear();
         $annonceMateriel = $entityManager->getRepository(AnnonceMateriel::class)->findAll();
+        #$annonceMateriel = $entityManager->getRepository(AnnonceMateriel::class)->findAll();
         return $this->render('home_page/index.html.twig', [
             'controller_name' => 'HomePageController',
             'annonceService' => $annonceService,
