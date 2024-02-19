48406fd5baeac93abbc3c6db91b11cbcadb6b168
zouari7u <sami.zouari5@etu.univ-lorraine.fr>
2024-02-19 15:27:57 +0100

    ajout de conditions sur username, username est unique -> deux utilisateurs ne peuvent avoir le même pseudo

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,6 +12,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[UniqueEntity(fields: ['email'], message: 'Il existe déjà un compte associé a cet e-mail.')]
+#[UniqueEntity(fields: ['username'], message: 'Ce pseudo est déjà utilisé par un autre utilisateur.')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -37,7 +38,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $first_name = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique : true)]
     private ?string $username = null;
 
     #[ORM\Column(nullable: true)]
