f5062019bfc1820c78d87e95d21ac977461badcb
khoualed2u <rayan.khoualed3@etu.univ-lorraine.fr>
2024-02-21 22:13:42 +0100

    test deconnexion fait

--- tests/TestsDeconnexionTest.php
+++ tests/TestsDeconnexionTest.php
@@ -2,12 +2,29 @@
 
 namespace App\Tests;
 
-use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class TestsDeconnexionTest extends TestCase
+class TestsDeconnexionTest extends WebTestCase
 {
     public function testSomething(): void
     {
-        $this->assertTrue(true);
+
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/login');
+
+        $this->assertResponseIsSuccessful();
+        $form = $crawler->filter('#login')->form();
+        $form['id'] = 'test2';
+        $form['password'] = '123456';
+        $client->submit($form);
+        $crawler = $client->followRedirect();
+        $this->assertRouteSame('app_home_page');
+
+        // Déconnectez l'utilisateur
+        $client->request('GET', '/logout');
+
+        // Vérifiez si l'utilisateur est déconnecté
+        $this->assertResponseRedirects('/'); // Assurez-vous que l'utilisateur est redirigé vers la page de connexion après la déconnexion
     }
 }
