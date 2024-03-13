8a81489d9585b2d92cb3dc99c63c12f581ec98a8
zouari7u <sami.zouari5@etu.univ-lorraine.fr>
2024-03-14 00:13:41 +0100

    modifications test emprunter service

--- tests/TestEmprunterServiceTest.php
+++ tests/TestEmprunterServiceTest.php
@@ -10,10 +10,38 @@ class TestEmprunterServiceTest extends TestCase
     {
         $client = static::createClient();
 
-        $client->request('POST', '/ajax/emprunt', ['annonceId' => '1', 'annonceType' => 'type_test']);
-        $response = $client->getResponse();
-       
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals('OK', $response->getContent());
+        // Accéder à la page de connexion
+        $crawler = $client->request('GET', '/login');
+
+        // Assurez-vous que la page de connexion est accessible
+        $this->assertResponseIsSuccessful();
+
+        $form = $crawler->filter('#login')->form();
+        $form['id'] = 'marcheToujours';
+        $form['password'] = '098765';
+        $client->submit($form);
+        $crawler = $client->followRedirect();
+        $this->assertRouteSame('app_home_page');
+        $this->assertTrue($client->getCrawler()->filter('html:contains("Mon profil")')->count() > 0);
+        
+        $crawler = $client->request('GET', '/profile');
+
+        // Vérifier que la connexion a réussi
+        $this->assertRouteSame('app_home_page');
+
+        $crawler = $client->request('GET', '/annonces/1');
+
+        $this->assertResponseIsSuccessful();
+
+        $this->assertCount(1, $crawler->filter('#emprunterBtn'));
+
+        $client->executeScript("document.querySelector('#emprunterBtn').click();");
+
+        $this->assertTrue($client->getResponse()->isRedirect('/confirmation_emprunt'), 'La redirection vers la page de confirmation d\'emprunt a échoué.');
+
+        $client->followRedirect();
+
+        // Vérifier que la page de confirmation d'emprunt est affichée (adapté selon votre application)
+        $this->assertRouteSame('confirmation_emprunt', 'La redirection n\'a pas abouti à la page de confirmation d\'emprunt.');
     }
 }
