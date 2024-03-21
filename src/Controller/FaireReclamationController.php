bf44cf0ffff085ff1907d0ed75f8c3e6f8e22ff2
Thomas <thomas.valen2@etu.univ-lorraine.fr>
2024-03-21 11:39:11 +0100

    reclamation en cours

--- src/Controller/FaireReclamationController.php
+++ src/Controller/FaireReclamationController.php
@@ -5,14 +5,38 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\CategorieReclamation;
+use App\Entity\Transaction;
+use App\Entity\User;
+
+
+
 
 class FaireReclamationController extends AbstractController
 {
     #[Route('/faire/reclamation', name: 'app_faire_reclamation')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('faire_reclamation/index.html.twig', [
+        if (!$this->getUser()) {
+            return $this->redirectToRoute('app_login');
+        }
+        $objets = $entityManager->getRepository(CategorieReclamation::class)->findAll();
+
+        $userId = $this->getUser()->getId();
+
+        // Récupérer les transactions où l'utilisateur connecté est le client ou le posteur de l'annonce
+        $transactions = $entityManager->getRepository(Transaction::class)->createQueryBuilder('t')
+            ->leftJoin('t.annonce', 'a')
+            ->where('t.client = :userId OR a.posteur = :userId')
+            ->setParameter('userId', $userId)
+            ->getQuery()
+            ->getResult();
+
+         return $this->render('faire_reclamation/index.html.twig', [
             'controller_name' => 'FaireReclamationController',
+            'objets' => $objets,
+            'transactions' => $transactions,
         ]);
     }
 
